f4326014727c3db0c828a91501ceec4770011e77
Thomas Schreiber <tom@insatsu.us>
2011-03-25 13:14:05 +0100

    use eschulte starter-kit style

--- init.el
+++ init.el
@@ -1,79 +1,24 @@
 ;;; init.el --- Where all the magic begins
 ;;
+;; Part of the Emacs Starter Kit
+;;
+;; This is the first thing to get loaded.
+;;
 ;; "Emacs outshines all other editing software in approximately the
 ;; same way that the noonday sun does the stars. It is not just bigger
 ;; and brighter; it simply makes everything else vanish."
 ;; -Neal Stephenson, "In the Beginning was the Command Line"
 
-;; Turn off mouse interface early in startup to avoid momentary display
-;; You really don't need these; trust me.
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(setq dotfiles-dir (file-name-directory (or load-file-name (buffer-file-name))))
 
-;; Load path etc.
+(add-to-list 'load-path (expand-file-name
+                         "lisp" (expand-file-name
+                                 "org" (expand-file-name
+                                        "src" dotfiles-dir))))
+;; Load up Org Mode and Babel
+(require 'org-install)
 
-(setq dotfiles-dir (file-name-directory
-                    (or (buffer-file-name) load-file-name)))
-(setq load-path (cons "~/.emacs.d/configs" load-path))
-(setq load-path (cons "~/.emacs.d/vendor" load-path))
-(defconst emacs-config-dir "~/.emacs.d/configs/")
-(defconst emacs-vendor-dir "~/.emacs.d/vendor/")
-
-;; Load up ELPA, the package manager
-
-(add-to-list 'load-path dotfiles-dir)
-
-(add-to-list 'load-path (concat dotfiles-dir "/elpa-to-submit"))
-
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-(setq package-user-dir (concat dotfiles-dir "elpa"))
-(setq custom-file (concat dotfiles-dir "custom.el"))
-
-(require 'package)
-(dolist (source '(("technomancy" . "http://repo.technomancy.us/emacs/")
-                  ("elpa" . "http://tromey.com/elpa/")))
-  (add-to-list 'package-archives source t))
-(package-initialize)
-(require 'starter-kit-elpa)
-
-;; These should be loaded on startup rather than autoloaded on demand
-;; since they are likely to be used in every session
-
-(require 'cl)
-(require 'saveplace)
-(require 'ffap)
-(require 'uniquify)
-(require 'ansi-color)
-(require 'recentf)
-
-;; backport some functionality to Emacs 22 if needed
-(require 'dominating-file)
-
-;; Load up starter kit customizations
-
-;;(require 'starter-kit-defuns)
-;;(require 'starter-kit-bindings)
-;;(require 'starter-kit-misc)
-;;(require 'starter-kit-registers)
-;;(require 'starter-kit-eshell)
-;;(require 'starter-kit-lisp)
-;;(require 'starter-kit-perl)
-;;(require 'starter-kit-ruby)
-;;(require 'starter-kit-js)
-
-;;(regen-autoloads)
-(load custom-file 'noerror)
-
-;; You can keep system- or user-specific customizations here
-(setq system-specific-config (concat dotfiles-dir system-name ".el")
-      user-specific-config (concat dotfiles-dir user-login-name ".el")
-      user-specific-dir (concat dotfiles-dir user-login-name))
-(add-to-list 'load-path user-specific-dir)
-
-(if (file-exists-p system-specific-config) (load system-specific-config))
-(if (file-exists-p user-specific-config) (load user-specific-config))
-(if (file-exists-p user-specific-dir)
-  (mapc #'load (directory-files user-specific-dir nil ".*el$")))
+;; load up the main file
+(org-babel-load-file (expand-file-name "starter-kit.org" dotfiles-dir))
 
 ;;; init.el ends here
