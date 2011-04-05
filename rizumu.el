40d86757d306c68320181b4bda66f2c5459a4ae5
Thomas Schreiber <tom@insatsu.us>
2011-04-05 12:45:52 +0200

    whitespace config

--- rizumu.el
+++ rizumu.el
@@ -1,3 +1,8 @@
+;; Using Emacs as a Server
+(server-start)
+
+;; ========== Apperance ==========
+
 ;; Fonts
 (set-face-attribute 'default nil :font "Inconsolata 12")
 
@@ -9,3 +14,28 @@
 
 ;;(color-theme-sanityinc)
 (color-theme-zenburn)
+
+;; ========== Place Backup Files in Specific Directory ==========
+
+;; Enable backup files.
+(setq make-backup-files t)
+
+;; Enable versioning with default values (keep five last versions, I think!)
+(setq version-control t)
+
+;; Save all backup file in this directory.
+(setq backup-directory-alist (quote ((".*" . "~/.emacs_backups/"))))
+
+;; ========== Enable Line and Column Numbering ==========
+
+;; Show line-number in the mode line
+(line-number-mode 1)
+
+;; Show column-number in the mode line
+(column-number-mode 1)
+
+;; ========== Whitespace =========
+
+(require 'whitespace)
+(setq standard-indent 4)
+(setq-default indent-tabs-mode nil)
