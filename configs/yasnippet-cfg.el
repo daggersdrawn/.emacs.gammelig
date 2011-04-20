da238b56762f161d01998bff5857499307d90412
Thomas Schreiber <tom@insatsu.us>
2011-04-20 17:44:40 +0200

    import snippents from the correct location

--- configs/yasnippet-cfg.el
+++ configs/yasnippet-cfg.el
@@ -1,6 +1,6 @@
 (require 'yasnippet) 
 
 (yas/initialize)
-(yas/load-directory "~/.emacs.d/vendor/yasnippet/snippets")
+(yas/load-directory "~/.emacs.d/snippets")
 
 (load-library "auto-complete-yasnippet")
