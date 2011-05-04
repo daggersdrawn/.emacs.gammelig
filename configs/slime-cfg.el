428abc1c70e81a897682d8ddd5260f5dfc5aed78
Thomas Schreiber <tom@insatsu.us>
2011-05-04 19:24:56 +0200

    drop unused config files

--- configs/slime-cfg.el
+++
@@ -1,9 +0,0 @@
-;; Setting CLISP for SLIME. 
-;; Don't forget to install 'clisp' binary on your system:
-;; Ex: $ sudo yum install clisp
-;; Instead of using the following line, you can execute in your
-;; buffer:
-;;      C-u M-x slime 
-;; and then:
-;;      clisp -K full
-(setq inferior-lisp-program "clisp -K full")
