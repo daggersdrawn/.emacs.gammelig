6129ddbb9f094aa0b2fa4e9239f5143db903d636
Thomas Schreiber <tom@insatsu.us>
2011-05-06 00:38:43 +0200

    adding this for now

--- languages/starter-kit-python.el
+++ languages/starter-kit-python.el
@@ -15,3 +15,25 @@
 ;;(add-to-list 'auto-mode-alist '("\\.pyx\\'" . cython-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.pxd\\'" . cython-mode))
 ;;(add-to-list 'auto-mode-alist '("\\.pxi\\'" . cython-mode))
+
+(require 'python)
+
+; indentation
+(defadvice python-calculate-indentation (around outdent-closing-brackets)
+  "Handle lines beginning with a closing bracket and indent them so that
+  they line up with the line containing the corresponding opening bracket."
+(save-excursion
+  (beginning-of-line)
+  (let ((syntax (syntax-ppss)))
+    (if (and (not (eq 'string (syntax-ppss-context syntax)))
+             (python-continuation-line-p)
+             (cadr syntax)
+             (skip-syntax-forward "-")
+             (looking-at "\\s)"))
+        (progn
+          (forward-char 1)
+          (ignore-errors (backward-sexp))
+          (setq ad-return-value (current-indentation)))
+      ad-do-it))))
+
+(ad-activate 'python-calculate-indentation)
