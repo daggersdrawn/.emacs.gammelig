4c7719e8507dc6e7b232211a3f346b0cf6989c5a
Thomas Schreiber <tom@insatsu.us>
2011-05-27 21:31:07 +0200

    gtd & org settings

--- configs/cfg_org.el
+++ configs/cfg_org.el
@@ -1,29 +1,113 @@
 ;; MobileOrg + Dropbox + org-mode
 ;; Don't forget to create your ~/Dropbox/MobileOrg folder!
 ;;
-;; Example of GTD task: 
+;; Example of GTD task:
 ;; ** TODO [#A] Buy milk :personal:
 ;;    DEADLINE: <2011-03-11 Fri>
 ;; Where:
 ;;    [#A] => Priority from 'A' to 'D'
-;;    :personal: => Tag 
+;;    :personal: => Tag
+;; See also:
+;; http://thread.gmane.org/gmane.emacs.orgmode/4832/focus=4854
 
-;; Set to the location of your Org files on your local system
-(setq org-directory "~/.org")
+;; keybindings
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cr" 'org-remember)
+(global-set-key "\C-cg" 'gtd)
+
 ;; Files for syncing
-(setq org-agenda-files (quote ( "~/.org/gtd.org")))
+(setq org-agenda-files
+    (list "~/org/gtd.org" "~/org/someday.org"))
 ;; Set to the name of the file where new notes will be stored
-(setq org-mobile-inbox-for-pull "~/.org/flagged.org")
+(setq org-mobile-inbox-for-pull "~/org/flagged.org")
 ;; Set to <your Dropbox root directory>/MobileOrg.
-(setq org-mobile-directory "~/Dropbox/MobileOrg")
+(setq org-mobile-directory "~/Dropbox/mobileorg")
 ;; Custom agenda view
 (setq org-mobile-force-id-on-agenda-items nil)
+;; return activates a link
+(setq org-return-follows-link t)
+;; Set keywords and agenda commands
+(setq org-todo-keywords '((type "TODO" "NEXT" "WAITING" "DONE")))
 (setq org-agenda-custom-commands
-       '(("w" todo "TODO")
-         ("h" agenda "" ((org-agenda-show-all-dates nil)))
-         ("W" agenda "" ((org-agenda-ndays 21)
-                         (org-agenda-show-all-dates nil)))
-         ("A" agenda ""
-          ((org-agenda-ndays 1)
-           (org-agenda-overriding-header "Today")))))
+    '(("w" todo "WAITING" nil)
+      ("n" todo "NEXT" nil)
+      ("d" "Agenda + Next Actions" ((agenda) (todo "NEXT"))))
+)
 
+(defun gtd ()
+   (interactive)
+   (find-file "~/org/gtd/gtd.org")
+ )
+
+;; I use org's tag feature to implement contexts.
+(setq org-tag-alist '(("STUDIO" . ?s)
+                      ("COMPUTER" . ?c)
+                      ("MAIL" . ?m)
+                      ("HOME" . ?h)
+                      ("FIELD" . ?f)
+                      ("READING" . ?r)
+                      ("DVD" . ?d)))
+
+;; I like to color-code task types.
+(setf org-todo-keyword-faces '(("NEXT" . (:foreground "yellow" :background "red" :bold t :weight bold))
+			       ("TODO" . (:foreground "cyan" :background "steelblue" :bold t :weight bold))
+			       ("WAITING" . (:foreground "yellow" :background "magenta2" :bold t :weight bold))
+			       ("DONE" . (:foreground "gray50" :background "gray30"))))
+
+;; I put the archive in a separate file, because the gtd file will
+;; probably already get pretty big just with current tasks.
+
+(setq org-archive-location "%s_archive::")
+
+;; Remember support. This creates several files:
+;;
+;;   ~/org/todo.org      Where remembered TODO's are stored.
+;;   ~/org/journal.org   Timestamped journal entries.
+;;   ~/org/remember.org  All other notes
+
+;; and a keybinding of "C-c r" for making quick notes from any buffer.
+
+;; These bits of Remembered information must eventually be reviewed
+;; and filed somewhere (perhaps in gtd.org, or in a project-specific
+;; org file.) The out-of-sight, out-of-mind rule applies here---if I
+;; don't review these auxiliary org-files, I'll probably forget what's
+;; in them.
+
+(setq org-reverse-note-order t)  ;; note at beginning of file by default.
+(setq org-default-notes-file "~/org/remember.org")
+(setq remember-annotation-functions '(org-remember-annotation))
+(setq remember-handler-functions '(org-remember-handler))
+(add-hook 'remember-mode-hook 'org-remember-apply-template)
+
+(setq org-remember-templates
+      '((?t "* TODO %?\n  %i\n  %a" "~/todo.org")
+        (?j "* %U %?\n\n  %i\n  %a" "~/journal.org")
+        (?i "* %^{Title}\n  %i\n  %a" "~/remember.org" "New Ideas")))
+
+;; My preferences. These are less related to GTD, and more to my
+;; particular setup. They are included here for completeness, and so
+;; that new org users can see a complete example org-gtd
+;; configuration.
+
+(setq org-return-follows-link t)
+(setq org-hide-leading-stars t)
+(setf org-tags-column -65)
+(setf org-special-ctrl-a/e t)
+
+(setq org-log-done t)
+(setq org-deadline-warning-days 14)
+(setq org-fontify-emphasized-text t)
+(setq org-fontify-done-headline t)
+(setq org-agenda-include-all-todo nil)
+(setq org-directory "~/org")
+(setq org-export-html-style "<link rel=stylesheet href=\"../e/freeshell2.css\" type=\"text/css\">")
+(setq org-export-with-section-numbers nil)
+(setq org-export-with-toc nil)
+(setq org-adapt-indentation nil)
+
+;; widen category field a little
+(setq org-agenda-prefix-format "  %-17:c%?-12t% s")
+
+(provide 'org-gtd)
+;;; org-gtd.el ends here
