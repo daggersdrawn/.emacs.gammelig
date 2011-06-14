e3c064c13cdda9608b814271c06df3c739142364
Thomas Schreiber <tom@insatsu.us>
2011-06-14 11:51:04 +0200

    Disable Emacs-Flymake for html mode.

--- configs/cfg_pyflake.el
+++ configs/cfg_pyflake.el
@@ -1,5 +1,8 @@
-;; While visiting a Python file, run: 
-;; M-x python-check or 
-;; C-c C-v or 
+;; While visiting a Python file, run:
+;; M-x python-check or
+;; C-c C-v or
 ;; C-c C-w
 (setq python-check-command "pyflakes")
+
+;; Disable Emacs-Flymake for html mode.
+(delete '("\\.html?\\'" flymake-xml-init) flymake-allowed-file-name-masks)
