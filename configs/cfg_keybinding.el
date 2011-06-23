0b532ce5e64267cf5f1d402844aee65abbac92f9
Thomas Schreiber <tom@insatsu.us>
2011-06-23 20:50:32 +0200

    anything keybinding

--- configs/cfg_keybinding.el
+++ configs/cfg_keybinding.el
@@ -3,7 +3,7 @@
 ;(global-set-key [f3] 'shell)
 ;(global-set-key [f4] 'indent-region)
 ; lusty
-(global-set-key "\C-x\C-f" 'lusty-file-explorer)
+(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
 (global-set-key (kbd "C-x b") 'lusty-buffer-explorer)
 ;(global-set-key [f5] 'lusty-file-explorer)
 ;(global-set-key [f6] 'lusty-buffer-explorer)
@@ -19,4 +19,5 @@
 ;; redo+
 ;(require 'redo+)
 (global-set-key (kbd "C-?") 'redo)
-(global-set-key "\C-xg" 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "M-SPC") 'anything)
