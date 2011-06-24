b75a79fe9dd7d535e7d2909a651f9d999e9c8b76
Thomas Schreiber <tom@insatsu.us>
2011-06-24 08:28:55 +0200

    keybindings

--- configs/cfg_keybinding.el
+++ configs/cfg_keybinding.el
@@ -2,21 +2,29 @@
 ;(global-set-key [f2] 'save-buffer)
 ;(global-set-key [f3] 'shell)
 ;(global-set-key [f4] 'indent-region)
-; lusty
-(global-set-key "\C-x\C-f" 'lusty-file-explorer)
+
+;; lusty
+(global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
 (global-set-key (kbd "C-x b") 'lusty-buffer-explorer)
-;(global-set-key [f5] 'lusty-file-explorer)
-;(global-set-key [f6] 'lusty-buffer-explorer)
-;		Copy and paste current line
+
+;; Copy and paste current line
 ;(global-set-key [f7] "\C-a\C- \C-n\M-w\C-y")
-;		Copy current line
+
+;; Copy current line
 ;(global-set-key [f8] "\C-a\C- \C-n\M-w")
-; Move between buffers
-;(global-set-key [C-right] 'next-buffer)
-;(global-set-key [C-left] 'previous-buffer)
-; Replace string
-;(global-set-key "\M-r" 'replace-string)
+
+;; Move between buffers
+(global-set-key [C-right] 'next-buffer)
+(global-set-key [C-left] 'previous-buffer)
+
 ;; redo+
-;(require 'redo+)
 (global-set-key (kbd "C-?") 'redo)
-(global-set-key "\C-xg" 'magit-status)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; pyflakes
+(global-set-key (kbd "M-m") 'flymake-display-err-menu-for-current-line)
+(global-set-key (kbd "M-p") 'flymake-goto-prev-error)
+(global-set-key (kbd "M-n") 'flymake-goto-next-error)
+
+;; anything
+(global-set-key (kbd "M-SPC") 'anything)
