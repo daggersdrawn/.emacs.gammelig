fec0b8802bd31dff6741761485e5c45817fbf14a
Thomas Schreiber <tom@insatsu.us>
2011-07-08 10:20:42 +0200

    iresize keybindings

--- configs/cfg_keybinding.el
+++ configs/cfg_keybinding.el
@@ -31,3 +31,9 @@
 
 ;; buffer-move
 (global-set-key (kbd "C-x 4") 'win-swap)
+
+;; iresize
+(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
+(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "S-C-<down>") 'shrink-window)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
