f7e74c82c9c0ad70765ffff70de23bb78bc3c18f
Thomas Schreiber <tom@insatsu.us>
2011-07-15 13:45:22 +0200

    drop unused keybindings

--- configs/cfg_keybinding.el
+++ configs/cfg_keybinding.el
@@ -1,17 +1,6 @@
-; Key bindings
-;(global-set-key [f2] 'save-buffer)
-;(global-set-key [f3] 'shell)
-;(global-set-key [f4] 'indent-region)
-
 ;; lusty
 (global-set-key (kbd "C-x C-f") 'lusty-file-explorer)
 (global-set-key (kbd "C-x b") 'lusty-buffer-explorer)
-
-;; Copy and paste current line
-;(global-set-key [f7] "\C-a\C- \C-n\M-w\C-y")
-
-;; Copy current line
-;(global-set-key [f8] "\C-a\C- \C-n\M-w")
 
 ;; Move between buffers
 (global-set-key [C-right] 'next-buffer)
