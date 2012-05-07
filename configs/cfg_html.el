bd9f8fa9b63e911ec1155b2d83de106e355d9cf0
Thomas Schreiber <tom@insatsu.us>
2012-05-07 15:32:37 +0200

    set default spaceing for html+js to 4

--- configs/cfg_html.el
+++ configs/cfg_html.el
@@ -2,3 +2,7 @@
 
 (require 'zencoding-mode)
 (add-hook 'sgml-mode-hook 'zencoding-mode) ;; start on any markup modes
+(add-hook 'html-mode-hook
+  (lambda ()
+    ;; Default indentation is usually 2 spaces, changing to 4.
+    (set (make-local-variable 'sgml-basic-offset) 4)))
--- configs/cfg_jshint.el
+++ configs/cfg_jshint.el
@@ -1,3 +1,7 @@
 (require 'flymake-jshint)
 (add-hook 'javascript-mode-hook
           (lambda () (flymake-mode t)))
+(add-hook 'javascript-mode-hook
+  (lambda ()
+    ;; Default indentation is usually 2 spaces, changing to 4.
+    (set (make-local-variable 'sgml-basic-offset) 4)))
