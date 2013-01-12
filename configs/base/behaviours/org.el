228df30c70457bc19a855ca9aa93f7c258268c7e
rizumu <tom@rizu.mu>
2013-01-12 22:42:38 +0100

    add org reference to sync to mobile

--- configs/base/behaviours/org.el
+++ configs/base/behaviours/org.el
@@ -21,7 +21,7 @@
 
 ;; Files for syncing
 (setq org-agenda-files
-    (list (concat org-directory "/gtd.org") (concat org-directory "/someday.org")  (concat org-directory "/journal.org")))
+    (list (concat org-directory "/gtd.org") (concat org-directory "/reference.org") (concat org-directory "/someday.org")  (concat org-directory "/journal.org")))
 
 ;; Set to the name of the file where new notes will be stored
 (setq org-mobile-inbox-for-pull (concat org-directory "/flagged.org"))
