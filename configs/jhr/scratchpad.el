3971a7f9ed0e250b51498833e9f7816bb51b1557
jhrr <jharrgt@gmail.com>
2013-05-28 11:24:45 +0200

    Couple of helm keybindings

--- configs/jhr/scratchpad.el
+++ configs/jhr/scratchpad.el
@@ -258,15 +258,13 @@ Repeated invocations toggle between the two most recently open buffers."
 (global-set-key (kbd "C-c I") 'find-user-init-file)
 
 (defun smarter-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line. If
+  "Move point back to indentation of beginning of line. Move
+point to the first non-whitespace character on this line. If
 point is already there, move to the beginning of the line.
 Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
+the beginning of the line.If ARG is not nil or 1, move forward
+ARG - 1 lines first. If point reaches the beginning or end of the
+buffer, stop there."
   (interactive "^p")
   (setq arg (or arg 1))
 
@@ -283,8 +281,9 @@ point reaches the beginning or end of the buffer, stop there."
 (global-set-key [remap move-beginning-of-line]
                 'smarter-move-beginning-of-line)
 
-(global-set-key (kbd "C-x C-f") 'helm-find-files)
-(global-set-key (kbd "C-c C-f") 'helm-locate)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-c r") 'helm-locate)
+(global-set-key (kbd "C-c h") 'helm-mini)
 
 
 (provide 'scratchpad.el)
