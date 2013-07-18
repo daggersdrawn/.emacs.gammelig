1a7b93c4353d5c3db313ba86fc6e82af4941378e
Thomas Schreiber <tom@rizu.fake>
2013-07-18 19:12:13 +0200

    Revert "setup wrap-region and trans keybinding"
    This reverts commit f782f91474b9f9f8b3ef94bcfcbb95066cf83f90.

--- configs/base/behaviours/wrapregion.el
+++
@@ -1,3 +0,0 @@
-(require 'wrap-region)
-(wrap-region-add-wrappers
- '(("{% trans \"" "\" %}" "e")))
--- configs/base/packages.el
+++ configs/base/packages.el
@@ -66,7 +66,6 @@
    undo-tree
    virtualenv
    worklog
-   wrap-region
    ;; wtf
    yaml-mode
    yasnippet
