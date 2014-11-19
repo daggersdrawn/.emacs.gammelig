2ed5b639655be5f33a821f0d3662b3e264528bba
Thomas Schreiber <rizumu@users.noreply.github.com>
2014-11-19 01:02:01 +0100

    cleanup org

--- configs/rizumu/behaviours/org.el
+++ configs/rizumu/behaviours/org.el
@@ -4,46 +4,46 @@
 ;; review these auxiliary org-files, I'll probably forget what's in them.
 (setq org-capture-templates
       '(("t" "todo" entry
-         (file+headline (concat org-directory "/gtd-inbox.org")
-                        "Tasks") "* TODO %?\n  %i\n  %a")
-        ("n" "note" entry
-         (file+headline (concat org-directory "/notes.org")
-                        "Notes to review") "* %^{Title}\n  %i\n  %a")
-        ("m" "morningpages" entry
-         (file+datetree (concat org-directory "/morningpages.org")
-                        "Morning pages") "* %?\nEntered on %U\n  %i\n  %a")))
+	 (file+headline (concat org-directory "/gtd-inbox.org")
+			"Tasks") "* TODO %?\n  %i\n  %a")
+	("n" "note" entry
+	 (file+headline (concat org-directory "/notes.org")
+			"Notes to review") "* %^{Title}\n  %i\n  %a")
+	("m" "morningpages" entry
+	 (file+datetree (concat org-directory "/morningpages.org")
+			"Morning pages") "* %?\nEntered on %U\n  %i\n  %a")))
 
 ;; Use org's tag feature to implement contexts.
 (setq org-tag-alist '((:startgroup)
-                      ("@buyonline" . ?b)
-                      ("@dl" . ?d)
-                      ("@errand" . ?e)
-                      ("@home" . ?h)
-                      ("@listen" . ?l)
-                      ("@mail" . ?m)
-                      ("@phone" . ?p)
-                      ("@read" . ?r)
-                      ("@studio" . ?s)
-                      ("@watch" . ?w)
-                      (:endgroup)
-                      ("DAILY" . ?D)
-                      ("WEEKLY" . ?W)
-                      ("MONTHLY" . ?M)
-                      ("QUARTERLY" . ?Q)
-                      ("YEARLY" . ?Y)
-                      ("RESEARCH" . ?R)
-                      ("PROJECT" . ?P)))
+		      ("@buyonline" . ?b)
+		      ("@dl" . ?d)
+		      ("@errand" . ?e)
+		      ("@home" . ?h)
+		      ("@listen" . ?l)
+		      ("@mail" . ?m)
+		      ("@phone" . ?p)
+		      ("@read" . ?r)
+		      ("@studio" . ?s)
+		      ("@watch" . ?w)
+		      (:endgroup)
+		      ("DAILY" . ?D)
+		      ("WEEKLY" . ?W)
+		      ("MONTHLY" . ?M)
+		      ("QUARTERLY" . ?Q)
+		      ("YEARLY" . ?Y)
+		      ("RESEARCH" . ?R)
+		      ("PROJECT" . ?P)))
 
 
 ;; Pomodoro time tracking in org
 ;; http://orgmode.org/worg/org-gtd-etc.html
 ;; http://thread.gmane.org/gmane.emacs.orgmode/29347
-(defun my-after-load-org ()
-	(add-to-list 'org-modules 'org-timer))
-(eval-after-load "org" '(my-after-load-org))
+;(defun my-after-load-org ()
+;	(add-to-list 'org-modules 'org-timer))
+;(eval-after-load "org" '(my-after-load-org))
 
 (setq org-timer-default-timer 25)
 
 (add-hook 'org-clock-in-hook
-          '(lambda () (if (not org-timer-current-timer)
-                     (org-timer-set-timer '(16)))))
+	  '(lambda () (if (not org-timer-current-timer)
+		     (org-timer-set-timer '(16)))))
